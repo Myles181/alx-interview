e816f115ac6a24ff5f8a39190f2f2f3abea38fa4
Emmanuel Cyril <cyrile450@gmail.com>
2023-08-20 22:19:17 +0000

    stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -54,7 +54,7 @@ def update_metrics(line, total_file_size, status_codes_stats):
     return total_file_size + line_info['file_size']
 
 
-def run():
+def main():
     '''Starts the log parser.
     '''
     line_num = 0
@@ -85,4 +85,4 @@ def run():
 
 
 if __name__ == '__main__':
-    run()
+    main()
